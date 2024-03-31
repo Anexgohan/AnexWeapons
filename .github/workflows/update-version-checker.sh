2ae91c91fa123f2efe73423710b8937938af9706
Anexgohan <monish05m@gmail.com>
2024-03-31 12:40:09 +0530

    test

--- .github/workflows/update-version-checker.sh
+++ .github/workflows/update-version-checker.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # this will update version checker version for Anex Weapons
+
+# go to the directory where the mod_info.json file is located
+cd ../..
+ls -R
 # get version info from "mod_info.json" file from the string "    "version": { "major": "0", "minor": "2", "patch": "9h" }," and save it to separate variables VC_major, VC_minor and VC_patch
-VC_major=$(jq -r '.version.major' ../../mod_info.json)
-VC_minor=$(jq -r '.version.minor' ../../mod_info.json)
-VC_patch=$(jq -r '.version.patch' ../../mod_info.json)
+VC_major=$(jq -r '.version.major' mod_info.json)
+VC_minor=$(jq -r '.version.minor' mod_info.json)
+VC_patch=$(jq -r '.version.patch' mod_info.json)
 # print the version info
 echo "Version Checker version of Anex Weapons is: major=$VC_major, minor=$VC_minor, patch=$VC_patch"
